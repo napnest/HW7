4c920957d7c2254ffd9813523cf502cfabfd420a
123456 <napnest@gmail.com>
2023-09-27 12:47:57 +0300

    Task 2 complete

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //todo Тут увольняем айтишников
+        for (int i = 1; i <=100 ; i++) {
+            if(i%2==0){
+                System.out.println("Уволен айтишник с номером: "+i);
+            }
+        }
     }
 }
